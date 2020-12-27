eb880361af611f5e46413ec143bc52d254270adf
Görkem <grkkmm1@gmail.com>
2020-12-27 20:39:19 +0300

    me.

---
+++ docs/web_modules/common/index-41f3a926.js
@@ -0,0 +1,9 @@
+function cubicOut(t) {
+    const f = t - 1.0;
+    return f * f * f + 1.0;
+}
+function quintOut(t) {
+    return --t * t * t * t * t + 1;
+}
+
+export { cubicOut as c, quintOut as q };
---
+++ docs/web_modules/svelte/easing.js
@@ -0,0 +1,1 @@
+export { q as quintOut } from '../common/index-41f3a926.js';
